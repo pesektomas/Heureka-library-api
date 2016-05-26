70ff0e6e9b228e3a204160b66f238394a9f411c2
Tomáš Pešek <tomas@sandra>
2016-05-26 19:22:08 +0200

    hodnoceni i uostatnich komponent

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -157,7 +157,6 @@ class ApiController extends Controller
 	{
 		$em = $this->getDoctrine()->getManager();
 		$workingBooks = self::getBookDqlWithRate($em)
-			->innerJoin(BookHolder::class, 'bh', 'WITH', 'bh.bookUniq = bu.code')
 			->innerJoin('bh.user', 'u')
 			->where('bh.to IS NULL AND u.email = :user')
 			->setParameter('user', $user)
@@ -231,7 +230,6 @@ class ApiController extends Controller
 	{
 		$em = $this->getDoctrine()->getManager();
 		$workingBooks = self::getBookDqlWithRate($em)
-			->innerJoin(BookHolder::class, 'bh', 'WITH', 'bh.bookUniq = bu.code')
 			->innerJoin('bh.user', 'u')
 			->where('u.email = :email')
 			->setParameter('email', $user)
